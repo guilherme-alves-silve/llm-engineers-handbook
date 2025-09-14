4bb8b0ba3782b3aa7265cd68b8aca5937bb5dab4
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2025-09-14 18:55:10 -0300

    :art: refactor: UPDATE

--- steps/etl/crawl_links.py
+++ steps/etl/crawl_links.py
@@ -11,7 +11,10 @@ from llm_engineering.domain.documents import UserDocument
 
 @step
 def crawl_links(user: UserDocument, links: list[str]) -> Annotated[list[str], "crawled_links"]:
-    dispatcher = CrawlerDispatcher.build().register_linkedin().register_medium().register_github()
+    dispatcher = (CrawlerDispatcher.build()
+                                   .register_linkedin()
+                                   .register_medium()
+                                   .register_github())
 
     logger.info(f"Starting to crawl {len(links)} link(s).")
 
